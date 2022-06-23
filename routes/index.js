85a62df96defabf19dc876313a62bc76207beb48
Atish-K <atish0518@gmail.com>
2022-06-23 12:23:36 +0530

    Comments_base

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ console.log('Router loaded');
 router.get('/',homeController.home);
 router.use('/users',require('./users'));
 router.use('/posts', require('./posts'));
+router.use('/comments', require('./comments'));
 
 // router.use('/profiles',require('./users'));
 // router.use('/post',require('./post'));
